cd8f3f90afbab3ddf6fe8e7a43edac149434fea2
Steven Majek <majek1@hotmail.com>
2020-12-05 15:41:33 +0100

    refactored server

--- server.ts
+++ server.ts
@@ -10,7 +10,7 @@ const path = require('path');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 const cors = require('cors');
-
+const mailSender = require('mailsender')
 
 const app = express();
 
@@ -37,13 +37,41 @@ app.use(cors(corsOptions));
 
 const port = process.env.PORT || 3001;
 app.use(bodyParser.json());
-app.use(express.static(__dirname + '../dist/stevenMajek'));
+// app.use(express.static(__dirname + 'http://localhost:4200'));
 app.get('/*', (req, res) => res.sendFile(path.join(__dirname)));
 
 const server = http.createServer(app);
 
 // send email
 
-module.exports = routes;
+app.post('/sendEmail', (req, res) => {
+
+    console.log(req.body);
+    const output = `
+    <p>You have a new contact request</p>
+    <h3>Contact details</h3>
+    <ul>
+        <li>Nombre: ${req.body.nombre}</li>
+        <li>Asunto: ${req.body.asunto}</li>
+        <li>Email: ${req.body.email}</li>
+    </ul>
+    <h3>Message</h3>
+    <p>${req.body.mensaje}</p>
+    `;
+    send(req.body.asunto, output);
+    res.send({ 'respuesta': 'enviado' });
+
+});
+
+
+function send(subject, message) {
+
+    mailSender.from('stevenmajek@gmail.com', 'lastkingsteven123')
+        .to('majek1@hotmail.com')
+        .body(subject, message, true)
+        .send();
+
+}
+
 
 server.listen(port, () => console.log('Listening to port', port));
